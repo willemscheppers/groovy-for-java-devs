54a2d1592b5494f0f62eca1c7164e30ad20bd036
willemscheppers <willem.scheppers@cegeka.com>
2023-10-11 10:35:12 +0200

    map literal

--- Gradle/GroovyConsole/src/main/groovy/console.groovy
+++ Gradle/GroovyConsole/src/main/groovy/console.groovy
@@ -1,14 +1,15 @@
-def credentials = [:]
-credentials << ["Beatrice": "cisco123"]
-credentials << ["Francis": "11111111"]
-credentials << ["Dollie": "pa55w0rd"]
-credentials << ["Marian": "asdfgh"]
-credentials << ["Lina": "11111"]
-credentials << ["Josephine": "123abc"]
-credentials << ["Matthew": "asdf"]
-credentials << ["Louisa": "centos"]
-credentials << ["Herman": "888888"]
-credentials << ["Claudia": "54321"]
+def credentials = [
+        "Beatrice" : "cisco123",
+        "Francis"  : "11111111",
+        "Dollie"   : "pa55w0rd",
+        "Marian"   : "asdfgh",
+        "Lina"     : "11111",
+        "Josephine": "123abc",
+        "Matthew"  : "asdf",
+        "Louisa"   : "centos",
+        "Herman"   : "888888",
+        "Claudia"  : "54321"
+]
 
 credentials.forEach((name, password) -> {
     System.out.println(name + " -> " + (name + ":" + password).bytes.encodeBase64())
