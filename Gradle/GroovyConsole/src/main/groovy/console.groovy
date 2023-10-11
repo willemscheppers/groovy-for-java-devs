3d72f3246a35b8023ad0b5a5580bd9e48db37f59
willemscheppers <willem.scheppers@cegeka.com>
2023-10-11 10:29:04 +0200

    transformation example (java)

--- Gradle/GroovyConsole/src/main/groovy/console.groovy
+++ Gradle/GroovyConsole/src/main/groovy/console.groovy
@@ -1,1 +1,15 @@
-"willems:myPassword".bytes.encodeBase64()
+Map<String, String> credentials = new HashMap<>();
+credentials.put("Beatrice", "cisco123");
+credentials.put("Francis", "11111111");
+credentials.put("Dollie", "pa55w0rd");
+credentials.put("Marian", "asdfgh");
+credentials.put("Lina", "11111");
+credentials.put("Josephine", "123abc");
+credentials.put("Matthew", "asdf");
+credentials.put("Louisa", "centos");
+credentials.put("Herman", "888888");
+credentials.put("Claudia", "54321");
+
+credentials.forEach((name,password) -> {
+    System.out.println(name + " -> "+ (name+":"+password).bytes.encodeBase64());
+});
