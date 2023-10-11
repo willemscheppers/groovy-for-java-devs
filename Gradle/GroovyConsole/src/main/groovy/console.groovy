0a3a433cee0a39190ca7754825eb9bd8f2cf5b60
willemscheppers <willem.scheppers@cegeka.com>
2023-10-11 10:30:21 +0200

    semicolons are optional

--- Gradle/GroovyConsole/src/main/groovy/console.groovy
+++ Gradle/GroovyConsole/src/main/groovy/console.groovy
@@ -1,15 +1,15 @@
-Map<String, String> credentials = new HashMap<>();
-credentials.put("Beatrice", "cisco123");
-credentials.put("Francis", "11111111");
-credentials.put("Dollie", "pa55w0rd");
-credentials.put("Marian", "asdfgh");
-credentials.put("Lina", "11111");
-credentials.put("Josephine", "123abc");
-credentials.put("Matthew", "asdf");
-credentials.put("Louisa", "centos");
-credentials.put("Herman", "888888");
-credentials.put("Claudia", "54321");
+Map<String, String> credentials = new HashMap<>()
+credentials.put("Beatrice", "cisco123")
+credentials.put("Francis", "11111111")
+credentials.put("Dollie", "pa55w0rd")
+credentials.put("Marian", "asdfgh")
+credentials.put("Lina", "11111")
+credentials.put("Josephine", "123abc")
+credentials.put("Matthew", "asdf")
+credentials.put("Louisa", "centos")
+credentials.put("Herman", "888888")
+credentials.put("Claudia", "54321")
 
 credentials.forEach((name,password) -> {
-    System.out.println(name + " -> "+ (name+":"+password).bytes.encodeBase64());
-});
+    System.out.println(name + " -> "+ (name+":"+password).bytes.encodeBase64())
+})
