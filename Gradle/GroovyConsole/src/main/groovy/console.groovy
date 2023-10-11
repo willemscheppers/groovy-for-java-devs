e2abd027b60890fad55ce594278b360911e0c388
willemscheppers <willem.scheppers@cegeka.com>
2023-10-11 10:36:56 +0200

    map literal syntax

--- Gradle/GroovyConsole/src/main/groovy/console.groovy
+++ Gradle/GroovyConsole/src/main/groovy/console.groovy
@@ -1,14 +1,14 @@
 def credentials = [
-        "Beatrice" : "cisco123",
-        "Francis"  : "11111111",
-        "Dollie"   : "pa55w0rd",
-        "Marian"   : "asdfgh",
-        "Lina"     : "11111",
-        "Josephine": "123abc",
-        "Matthew"  : "asdf",
-        "Louisa"   : "centos",
-        "Herman"   : "888888",
-        "Claudia"  : "54321"
+        Beatrice : "cisco123",
+        Francis  : "11111111",
+        Dollie   : "pa55w0rd",
+        Marian   : "asdfgh",
+        Lina     : "11111",
+        Josephine: "123abc",
+        Matthew  : "asdf",
+        Louisa   : "centos",
+        Herman   : "888888",
+        Claudia  : "54321"
 ]
 
 credentials.forEach((name, password) -> {
